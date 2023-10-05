dba597442c8c4091f1a169a49bddfbcf3e0160e2
technologicaljerry <cserajatverma@gmail.com>
2023-10-05 22:48:09 +0530

    Update forgot-password.module.ts

--- angular-client/src/app/forgot-password/forgot-password.module.ts
+++ angular-client/src/app/forgot-password/forgot-password.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ForgotPasswordRoutingModule } from './forgot-password-routing.module';
+import { ForgotPasswordComponent } from './forgot-password.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ForgotPasswordComponent
+  ],
   imports: [
     CommonModule,
     ForgotPasswordRoutingModule
