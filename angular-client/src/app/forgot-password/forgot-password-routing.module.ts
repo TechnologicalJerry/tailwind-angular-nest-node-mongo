f82ffec0f785f8cdfe79f828a9e9c8c20d7e296c
technologicaljerry <cserajatverma@gmail.com>
2023-10-06 23:58:56 +0530

    Update forgot-password-routing.module.ts

--- angular-client/src/app/forgot-password/forgot-password-routing.module.ts
+++ angular-client/src/app/forgot-password/forgot-password-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ForgotPasswordComponent } from './forgot-password.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ForgotPasswordComponent, pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
