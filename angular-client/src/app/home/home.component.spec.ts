8361ae256df198398bed37f2d6635f4d6f1df8a3
technologicaljerry <cserajatverma@gmail.com>
2023-10-07 23:49:26 +0530

    Update home.component.spec.ts

--- angular-client/src/app/home/home.component.spec.ts
+++ angular-client/src/app/home/home.component.spec.ts
@@ -3,6 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HomeComponent } from './home.component';
 
 describe('HomeComponent', () => {
+
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
 
@@ -18,4 +19,5 @@ describe('HomeComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
 });
