f56185c9da21f1afdc5e14baf85591985b3082d2
technologicaljerry <cserajatverma@gmail.com>
2023-10-07 23:49:12 +0530

    Update login.component.spec.ts

--- angular-client/src/app/login/login.component.spec.ts
+++ angular-client/src/app/login/login.component.spec.ts
@@ -3,6 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LoginComponent } from './login.component';
 
 describe('LoginComponent', () => {
+
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
 
@@ -18,4 +19,5 @@ describe('LoginComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
 });
